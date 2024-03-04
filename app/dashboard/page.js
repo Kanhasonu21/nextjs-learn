81bd855fa0e25c460e8b241bcd17d673a848cff1
Kanhaiya Kumar <kanhaiyak42@gmail.com>
2024-03-04 14:51:20 +0530

    open graph removed

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,12 +3,13 @@ import Dashboard from "./components/Dashboard";
 
 
 export const metadata = {
+    metadataBase: new URL('http://localhost:3000'),
     title:"Add New Task",
     description:'The New Way to develop web app',
-    openGraph:{
-        title: 'this is open graph title for testing',
-        description: 'Some description',
-    }
+    // openGraph:{
+    //     title: 'this is open graph title for testing',
+    //     description: 'Some description',
+    // }
 }
 export default async function DashboardPage() {
 
